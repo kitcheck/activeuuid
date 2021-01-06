b87a0f0133169750a1eff76d456d7228ba707c60
Erin Szarpa <erin.szarpa@kitcheck.com>
2021-01-06 17:45:01 -0500

    [For #176036888] - update quote method for Rails 5.2

--- spec/support/migrate/20141017204945_add_array_to_articles.rb
+++ spec/support/migrate/20141017204945_add_array_to_articles.rb
@@ -1,4 +1,4 @@
-class AddArrayToArticles < ActiveRecord::Migration
+class AddArrayToArticles < ActiveRecord::Migration[4.2]
   def change
     add_column :articles, :some_array, :integer, array: true
   end
